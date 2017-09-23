582bc5aa7c5278c9118f0f28a82ff7e0be75889e
chrischne <chrischne@gmail.com>
2017-09-23 16:02:31 +0200

    connecting to specific ip

--- sketch.js
+++ sketch.js
@@ -1,4 +1,4 @@
-var muse = musedata.fake();
+var muse = musedata.connect('http://192.168.1.20:8081');
 
 function setup() {
   createCanvas(800,600);
@@ -8,9 +8,9 @@ function draw() {
   background(255);
   
   var beta = muse.getBeta();
-  var radius = map(beta,0,0.3,0,300);
+  var d = map(beta,0,0.3,0,300);
 
   console.log(beta);
   fill('blue');
-  ellipse(width/2,height/2,radius,radius);
+  ellipse(width/2,height/2,d,d);
 }
