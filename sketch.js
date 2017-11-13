7565e3c115f23d17b63ae2d73a0b62562e62eed6
chrischne <chrischne@gmail.com>
2017-11-13 22:41:54 +0100

    set to fake data

--- sketch.js
+++ sketch.js
@@ -1,4 +1,5 @@
-var muse = musedata.connect('http://192.168.1.20:8081');
+//var muse = musedata.connect('http://192.168.1.20:8081');
+var muse = musedata.fake();
 
 var ypos = 0;
 var dir = 0;
@@ -13,7 +14,6 @@ function setup() {
     //initialize position and direction
     ypos = height / 2;
     dir = 0;
-
 }
 
 function draw() {
