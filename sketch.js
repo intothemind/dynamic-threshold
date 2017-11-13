a94aac82dfcc3e3cf3789b15e43b7512b3a8c17b
chrischne <chrischne@gmail.com>
2017-11-13 22:11:03 +0100

    add dynamic threshold

--- sketch.js
+++ sketch.js
@@ -1,16 +1,32 @@
 var muse = musedata.connect('http://192.168.1.20:8081');
 
+var ypos = 0;
+var dir = 0;
+
+//variable storing dynamic threshold
+var thresh = dynamicThreshold();
+
 function setup() {
-  createCanvas(800,600);
+  createCanvas(windowWidth,windowHeight);
+
+  //initialize position and direction
+  ypos = height/2;
+  dir = 0;
+
 }
 
 function draw() {
   background(255);
   
-  var beta = muse.getBeta();
-  var d = map(beta,0,0.3,0,300);
+  var a = muse.getAlpha();
+  var threshold = thresh.threshold(a);
+  console.log(threshold);
 
-  console.log(beta);
-  fill('blue');
-  ellipse(width/2,height/2,d,d);
+
+  //update position
+  ypos = ypos + dir;
+
+  fill('steelblue');
+  noStroke();
+  ellipse(width/2,ypos,20,20);
 }
